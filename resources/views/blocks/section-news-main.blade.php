2731fbc2e9e355b8d70c8a1f147340dc7338bd26
sxtim <sxtimx@gmail.com>
2024-08-01 15:41:03 +0300

    refactor section news, catalog-news__cards-wrapper

--- resources/views/blocks/section-news-main.blade.php
+++ resources/views/blocks/section-news-main.blade.php
@@ -1,7 +1,7 @@
 <section class="news-main section">
     <div class="container">
         <div class="news-main__title title-s">Новостной блог</div>
-        <div class="news-main__wrapper">
+        <div class="catalog-news__cards-wrapper">
             @foreach($news as $itemNews)
                 @include('blocks/card-news')
             @endforeach
@@ -9,7 +9,7 @@
                 <div class="card-news-main__desc">
                     <h2 class="card-news-main__title">Новостной блог</h2>
                     <h3 class="card-news-main__sub-title">Раздел с самыми актуальными новостями в банной сфере. Про новинки в печной индустрии. Все самое полезное и интересное.</h3>
-                    <a href="{{ route('news.index') }}" class="card-news-main__link btn btn-medium">ВСЕ  НОВОСТИ</a>
+                    <a href="{{ route('news.index') }}" class="card-news-main__link btn btn-medium">все новости</a>
                 </div>
             </article>
         </div>
