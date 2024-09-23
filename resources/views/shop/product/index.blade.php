a31c6e5c5bd1f128b177c885096ed2505a0590b0
sxtim <sxtimx@gmail.com>
2024-09-23 18:16:33 +0300

    added filters catalog

--- resources/views/shop/product/index.blade.php
+++ resources/views/shop/product/index.blade.php
@@ -10,6 +10,100 @@
             <h1 class="catalog__title title-s">
                 {{ $category->name }}
             </h1>
+            <div class="catalog__filter-container">
+                        <details class="catalog__filter-details">
+                            <summary>Your favourite cars list</summary>
+                            <form>
+                                <fieldset>
+                                    <legend>Cars</legend>
+                                    <ul>
+                                        <li>
+                                            <label for="bmw">BMW<input type="checkbox" id="bmw" name="bmw"
+                                                                       value="bmw"></label>
+                                        </li>
+                                        <li>
+                                            <label for="citroen">Citroen
+                                                <input type="checkbox" id="citroen" name="citroen"
+                                                       value="citroen"></label>
+                                        </li>
+                                        <li>
+                                            <label for="skoda">Skoda
+                                                <input type="checkbox" id="skoda" name="skoda" value="skoda"></label>
+                                        </li>
+                                        <li>
+                                            <label for="volvo">Volvo
+                                                <input type="checkbox" id="volvo" name="volvo" value="volvo"></label>
+                                        </li>
+                                    </ul>
+                                </fieldset>
+                            </form>
+                        </details>
+                        <details class="catalog__filter-details">
+                            <summary>Your favourite cars list</summary>
+                            <form>
+                                <fieldset>
+                                    <legend>Cars</legend>
+                                    <ul>
+                                        <li>
+                                            <label for="bmw">BMW<input type="checkbox" id="bmw" name="bmw"
+                                                                       value="bmw"></label>
+                                        </li>
+                                        <li>
+                                            <label for="citroen">Citroen
+                                                <input type="checkbox" id="citroen" name="citroen"
+                                                       value="citroen"></label>
+                                        </li>
+                                        <li>
+                                            <label for="skoda">Skoda
+                                                <input type="checkbox" id="skoda" name="skoda" value="skoda"></label>
+                                        </li>
+                                        <li>
+                                            <label for="volvo">Volvo
+                                                <input type="checkbox" id="volvo" name="volvo" value="volvo"></label>
+                                        </li>
+                                    </ul>
+                                </fieldset>
+                            </form>
+                        </details>
+                    <details class="catalog__filter-details">
+                        <summary>Your favourite cars list</summary>
+                        <form>
+                            <fieldset class="filter-el">
+                                <legend>Cars</legend>
+                                <ul>
+                                    <li>
+                                        <label for="bmw">BMW<input type="checkbox" id="bmw" name="bmw"
+                                                                   value="bmw"></label>
+                                    </li>
+                                    <li>
+                                        <label for="citroen">Citroen
+                                            <input type="checkbox" id="citroen" name="citroen" value="citroen"></label>
+                                    </li>
+                                    <li>
+                                        <label for="skoda">Skoda
+                                            <input type="checkbox" id="skoda" name="skoda" value="skoda"></label>
+                                    </li>
+                                    <li>
+                                        <label for="volvo">Volvo
+                                            <input type="checkbox" id="volvo" name="volvo" value="volvo"></label>
+                                    </li>
+                                    <li>
+                                        <label for="volvo">Volvo
+                                            <input type="checkbox" id="volvo" name="volvo" value="volvo"></label>
+                                    </li>
+                                    <li>
+                                        <label for="volvo">Volvo
+                                            <input type="checkbox" id="volvo" name="volvo" value="volvo"></label>
+                                    </li>
+                                    <li>
+                                        <label for="volvo">Volvo
+                                            <input type="checkbox" id="volvo" name="volvo" value="volvo"></label>
+                                    </li>
+                                </ul>
+                            </fieldset>
+                        </form>
+                    </details>
+            </div>
             <div class="catalog__wrapper">
                 <div class="catalog__cards-wrapper">
                     @foreach($products as $product)
@@ -17,7 +111,7 @@
                     @endforeach
                 </div>
                 <div class="catalog-pagination__container">
-{{--                    @@include('blocks/pagination-page.html')--}}
+                    {{--                    @@include('blocks/pagination-page.html')--}}
                 </div>
             </div>
         </div>
